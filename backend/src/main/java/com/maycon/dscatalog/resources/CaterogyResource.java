acd0744474b5843b49d820e5ce348097d6b29fa5
Maycon J Pereira <mayconjovan@gmail.com>
2021-05-15 20:44:13 -0300

    DTO

--- backend/src/main/java/com/maycon/dscatalog/resources/CaterogyResource.java
+++ backend/src/main/java/com/maycon/dscatalog/resources/CaterogyResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.maycon.dscatalog.entities.Category;
+import com.maycon.dscatalog.dto.CategoryDTO;
 import com.maycon.dscatalog.services.CategoryService;
 
 @RestController
@@ -19,8 +19,8 @@ public class CaterogyResource {
 	private CategoryService service;
 		
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
--- backend/src/main/java/com/maycon/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/maycon/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.maycon.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.maycon.dscatalog.dto.CategoryDTO;
 import com.maycon.dscatalog.entities.Category;
 import com.maycon.dscatalog.repositories.CategoryRepository;
 
@@ -14,9 +16,11 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
 	}
 }
