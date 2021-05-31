932ba8861492f0cc931f5cc57f242fc7951704e3
Maycon J Pereira <mayconjovan@gmail.com>
2021-05-31 13:28:06 -0300

    User Crud

---
+++ backend/src/main/java/com/maycon/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.maycon.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maycon.dscatalog.entities.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
---
+++ backend/src/main/java/com/maycon/dscatalog/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.maycon.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maycon.dscatalog.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
