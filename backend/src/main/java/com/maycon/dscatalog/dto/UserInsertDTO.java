a23297bf20b04604e1478a0781be85a0f6791b51
Maycon J Pereira <mayconjovan@gmail.com>
2021-05-31 14:57:03 -0300

    Custom constraint validator

--- backend/src/main/java/com/maycon/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/maycon/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.maycon.dscatalog.dto;
 
+import com.maycon.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 	
--- backend/src/main/java/com/maycon/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/maycon/dscatalog/repositories/UserRepository.java
@@ -7,5 +7,7 @@ import com.maycon.dscatalog.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	User findByEmail(String email);
 
 }
